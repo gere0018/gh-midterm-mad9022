086e0c3335c12c4c1f896b84272ed6e63bc3bfd5
Nehmat <gere0018@algonquinlive.com>
2015-03-20 00:43:27 -0400

    added hammer js, temporary li s in html, overly and module window. have bug, contacts function not getting called.

--- js/script.js
+++ js/script.js
@@ -1,91 +1,62 @@
+//TODO:add hammer.js instead of handle touch and handle nav and get it functional in spite of event .current target.
+//TODO:js create UL with max 12 li and loop through contacts and display them in it.
+//TODO: add all contacts to local storage. save contact info as object use JSON.
+//TODO: add a dynamic map, register and get key.
+//TODO:create modal window that will display contact info.
+//TODO: double tap list view goes to map view, markers, contacts lat and long
+//TODO: create back button functionality.
 var app1_gere0018 = {
     pages:[],
     numPages:0,
-    links:[],
-    numLinks:0,
-    pageWrapper: "",
-    toggleMenuIcon:"",
-    verticalMenu: "",
-    deviceContacts: "",
+    lists:[],
+    numLists:0,
     initialize: function() {
-        app1_gere0018.bindEvents();
+      app1_gere0018.bindEvents();
     },
     bindEvents: function() {
-      document.addEventListener('deviceready', app1_gere0018.onDeviceReady, false);
+      //document.addEventListener('deviceready', app1_gere0018.onDeviceReady, false);
       document.addEventListener("DOMContentLoaded", app1_gere0018.onDomReady, false);
     },
-    onDeviceReady: function() {
-        //When device is ready read the contacts on the device
-     var options = new ContactFindOptions( );
-        options.filter = "";  //leaving this empty will find return all contacts
-        options.multiple = true;  //return multiple results
-        var filter = ["displayName"]; //an array of fields to compare against the options.filter
-        navigator.contacts.find(filter, app1_gere0018.contactsSuccess, app1_gere0018.contactsError, options);
-    },
+//    onDeviceReady: function() {
+//        //When device is ready read the contacts on the device
+//     var options = new ContactFindOptions( );
+//        options.filter = "";  //leaving this empty will find return all contacts
+//        options.multiple = true;  //return multiple results
+//        var filter = ["displayName"];
+//        navigator.contacts.find(filter, app1_gere0018.contactsSuccess, app1_gere0018.contactsError, options);
+//    },
     // Update DOM on a Received Event
     onDomReady: function(id) {
       app1_gere0018.prepareNavigation();
     },
 
     prepareNavigation:function(){
-        //add Listeners to toggle menu icon
-       toggleMenuIcon = document.querySelector("#toggle-menu");
-        if(app1_gere0018.detectTouchSupport( )){
-            toggleMenuIcon.addEventListener("touchend", app1_gere0018.handleTouch);
-         }
-       toggleMenuIcon.addEventListener("click", app1_gere0018.showMenu);
-
+        //add hammer Listeners to toggle menu icon
        pages = document.querySelectorAll('[data-role="page"]');
 	   numPages = pages.length;
-	   links = document.querySelectorAll(".button");
-	   numLinks = links.length;
-        //loop through my links and addlisteners to touch and click events
-	   for(var i=0;i<numLinks; i++){
-           if(app1_gere0018.detectTouchSupport( )){
-                links[i].addEventListener("touchend", app1_gere0018.handleTouch, false);
-            }
-           links[i].addEventListener("click", app1_gere0018.handleNav, false);
+	   lists = document.querySelectorAll(".tab");
+	   numLists = lists.length;
+        //loop through my lists and add hammer
+	   for(var i=0;i<numLists; i++){
+           var hammerLists = new Hammer(lists[i]);
+           hammerLists.on('tap', app1_gere0018.handleNav);
+            //FUTURE:add listener to browser's back button
 
-       }
-        //add listener to browser's back button
-       window.addEventListener("popstate", app1_gere0018.browserBackButton, false);
-        console.log("Calling loadPage");
-        //load the first page with url=null
-	   app1_gere0018.loadPage(null);
-        //Add touch/click listener to contacts button
-        var selectBtn = document.querySelector("#selectBtn");
-        if(app1_gere0018.detectTouchSupport( )){
-            selectBtn.addEventListener("touchend", app1_gere0018.handleTouch);
-         }
-        selectBtn.addEventListener("click", app1_gere0018.selectRandomContact);
+        }
+         //load the first page with url=null
+           app1_gere0018.loadPage(null);
+         //FUTURE: remove this when testing on device. temp for browser testing.
+           app1_gere0018.contactsSuccess;
 
     },
-    //Transform the touch event into a mouse event "click":
-    handleTouch:function (ev){
-      ev.preventDefault();//prevent 300 ms delay
-      ev.stopImmediatePropagation();
-      var touch = ev.changedTouches[0]; //this is the first object touched
-      var newEvt = document.createEvent("MouseEvent");
-      //old method works across browsers, though it is deprecated.
-      newEvt.initMouseEvent("click", true, true, window, 1, touch.screenX, touch.screenY, touch.clientX, touch.clientY);
-      ev.currentTarget.dispatchEvent(newEvt);
-      //send the touch to the click handler
-    },
-    //handle the click event
     handleNav:function (ev){
         ev.preventDefault();// preventing page reload
-        var href = ev.currentTarget.href;
+        var href = ev.target.href;
         var parts = href.split("#");//returns an array with 2 strings, the string before # and the string after the #.
         app1_gere0018.loadPage( parts[1] );
+        app1_gere0018.contactsSuccess;
         return false;
-    },
-    showMenu:function(){
-        //change toggle menu icon to an x shape when clicked
-        toggleMenuIcon.classList.toggle("x-toggle-menu");
-        verticalMenu = document.querySelector(".verticalMenu");
-        pageWrapper = document.querySelector("#pageWrapper");
-        pageWrapper.classList.toggle("pageWrapperPushedLeft");
-        verticalMenu.classList.toggle("OpenverticalMenu");
+
     },
 
     //Deal with history API and switching divs
@@ -105,155 +76,69 @@ var app1_gere0018 = {
               if(pages[i].id == url){
                   pages[i].className = "activePage pt-page-rotateInNewspaper pt-page-delay500";
 
-                  //making vertical menu disapper when we select a tab.
-                  if(verticalMenu.className == "verticalMenu OpenverticalMenu"){
-                      verticalMenu.classList.toggle("OpenverticalMenu");
-                      toggleMenuIcon.classList.toggle("x-toggle-menu");
-                      pageWrapper.classList.toggle("pageWrapperPushedLeft");
-                  }
-                  if(pages[i].id == "location"){
-                  app1_gere0018.setLocation();
-                  }
+                    setTimeout(function(){
+                        window.scrollTo(0,0);
+                    },100);
+//                      if(pages[i].id == "location"){
+//                      app1_gere0018.setLocation();
+//                      }
 
                 history.pushState(null, null, "#" + url);
-              }else{
-                  //Out Page:for the other page that was active and will be replaced
-                  var classes = pages[i].getAttribute("class");
-                  if (classes && (-1 !== classes.indexOf("activePage"))){
-                       pages[i].className = "activePage pt-page-rotateOutNewspaper";
-                    setTimeout(function(pg){
-                       pg.classList.remove("activePage");
-                       pg.classList.remove("pt-page-rotateOutNewspaper");
-                        }, 1000, pages[i]);
-                   }
-                }
+                }else{
+                    //Out Page:for the other page that was active and will be replaced
+                      var classes = pages[i].getAttribute("class");
+                      if (classes && (-1 !== classes.indexOf("activePage"))){
+                           pages[i].className = "activePage pt-page-rotateOutNewspaper";
+                        setTimeout(function(pg){
+                           pg.classList.remove("activePage");
+                           pg.classList.remove("pt-page-rotateOutNewspaper");
+                            }, 1000, pages[i]);
+                       }
+                    }
             }
 
-            }
-    //loop through links
-      for(var t=0; t < numLinks; t++){
-      links[t].classList.remove("activeTab");
-        //location is a property of the window object that returns current location
-          //url of the document.
-              if(links[t].href == window.location.href){
-                links[t].classList.add("activeTab");
-              }
-
-            }
-        },
-    //Need a listener for the popstate event to handle the back button
-    browserBackButton:function (ev){
-      url = window.location.hash;  //hash will include the "#" + the string after it.
-      //update the visible div and the active tab
-      for(var i=0; i < numPages; i++){
-          if(("#" + pages[i].id) == url){
-            pages[i].className = "activePage";
-          }else{
-            pages[i].classList.remove("activePage");
-          }
-      }
-      for(var t=0; t < numLinks; t++){
-        links[t].classList.remove("activeTab");
-        if(links[t].href == window.location.href){
-          links[t].classList.add("activeTab");
         }
-      }
+        //loop through lists
+          for(var t=0; t < numLists; t++){
+                lists[t].firstChild.classList.remove("activeTab");
+            //location is a property of the window object that returns current location
+              //url of the document.
+                  if(lists[t].firstChild.href == window.location.href){
+                    lists[t].firstChild.classList.add("activeTab");
+                  }
+
+            }
+            app1_gere0018.contactsSuccess;
+
     },
 
-//Test for browser support of touch events
-    detectTouchSupport:function ( ){
-      msGesture = navigator && navigator.msPointerEnabled && navigator.msMaxTouchPoints > 0 && MSGesture;
-      var touchSupport = (("ontouchstart" in window) || msGesture || (window.DocumentTouch && document instanceof DocumentTouch));
-      return touchSupport;
-    },
-    //Using Cordova Geolocation API to get current Location.
-    setLocation: function(){
-        console.log("location called");
-        if( navigator.geolocation ){
-        var getLocation = {enableHighAccuracy: false, timeout:60000, maximumAge:60000};
-        navigator.geolocation.getCurrentPosition( app1_gere0018.reportPosition, app1_gere0018.gpsError, getLocation);
-        //If it doesn't alert the user with the following message.
-        }else{
-            alert("OOPS!! your browser needs to be updated and currently does not support location based services.")
-        }
-    },
-    //success function
-    reportPosition:function( position ){
-         app1_gere0018.drawImage(position);
-        },
-
-    drawImage:function(position){
-      var canvas = document.querySelector("canvas");
-      var context = canvas.getContext('2d');
-      var imageObj = new Image();
-     imageObj.onload = function() {
-        context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
-      };
-     imageObj.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + position.coords.latitude + ","+ position.coords.longitude + "&zoom=14&size=400x400&markers=color:red%7C" + position.coords.latitude + ","+ position.coords.longitude + "&key=AIzaSyAOXUJlnbNxvV7HRtrslv_vFBT7tCJ4VZc";
-// append the canvas to the div with id output.
-     var output = document.querySelector("#output");
-     var msg = document.querySelector("#msg");
-     msg.innerHTML = "<strong>Your current location is:</strong><br/>" +
-         "Latitude: " + position.coords.latitude + "&deg;<br/>" +
-         "Longitude: " + position.coords.longitude + "&deg;<br/><br/>" ;
-    //after loading the map image, remove the message asking the user to wait for loading.
-    var waitMsg = document.querySelector("#waitMsg");
-    if(waitMsg){
-        waitMsg.parentNode.removeChild(waitMsg);
-    }
-},
-    gpsError:function ( error ){
-      var errors = {
-        1: 'Permission denied',
-        2: 'Position unavailable',
-        3: 'Request timeout'
-      };
- //in case of erros the following function gives an explanation of type of error to the user.
-      alert("Error: " + errors[error.code]);
-    },
-    selectRandomContact: function (){
-        //generate random number
-        this.innerHTML = "Change Contact";
-        var randomNumber = Math.floor(Math.random()* deviceContacts.length);
+    contactsSuccess: function (){
+        console.log("success");
         var contactsOutput = document.querySelector("#contactsOutput");
-       contactsOutput.innerHTML = "<h4>Your Emergency Contact's info: </h4></br>" +
-               "<p>Name: " +  deviceContacts[randomNumber].displayName + "<p></br>";
-        // Display Contact's Phone number ******************************************
-        var contactNumber;
-          if(deviceContacts[randomNumber].phoneNumbers== null){
-              contactNumber= "This contact has no saved number";
-          }else{
-              contactNumber= deviceContacts[randomNumber].phoneNumbers[0].value;
-          }
-         contactsOutput.innerHTML += "<p>Phone number: " + contactNumber  + "<p></br>";
-
-        // Display Contact's address ***********************************************
-        var contactAddress;
-         if(deviceContacts[randomNumber].addresses== null) {
-            contactAddress = "This contact has no saved address";
-         }else{
-           contactAddress = deviceContacts[randomNumber].addresses[0].formatted;
+         //for( var i=0; i<contacts.length; i++){
+           // contactsOutput.innerHTML = "<li [data-ref="i"]>" + deviceContacts[i].displayName
+                                        //+ "</li>";
+        var listItem = document.querySelectorAll('[data-ref^="li-"]');
+         for( var i=0; i<listItem.length; i++){
+             var hammerListItem = new Hammer(listItem[i]);
+             hammerListItem.on('tap', app1_gere0018.displayContact);
+             hammerListItem.on('doubletap', app1_gere0018.displayLocation);
          }
 
-         contactsOutput.innerHTML += "<p>Address: " +  contactAddress + "<p></br>";
-        var contactPhoto;
-        if(deviceContacts[randomNumber].photos== null) {
-           contactPhoto = "This contact has no saved photo";
-         }else{
-           contactPhoto = deviceContacts[randomNumber].photos[0].value;
-           contactsOutput.innerHTML += "<img src = " + contactPhoto + "></img></br>";
-         }
 
 
     },
+   displayContact: function(){
+      console.log("contact display");
+
+   },
+    displayLocation: function(){
+       alert(this);
+
+   },
     contactsError:function (){
         alert("sorry !! we are not able to load your contact right now!!")
 
-    },
-
-    contactsSuccess: function (contacts){
-//saved the contacts returned from success function in the variable deviceContacts
-        deviceContacts = contacts;
     }
 
  };
